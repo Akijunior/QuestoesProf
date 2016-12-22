f58289c6b92db68bfafc08e750ed60cddf6ab42f
Akijunior <suitsu19@gmail.com>
2016-12-22 20:11:13 -0200

    Create Caelum_Cap02.java

---
+++ Caelum_Cap02.java
@@ -0,0 +1,18 @@
+package Application;
+
+public class Caelum {
+	
+	public static void main(String[] args) {
+		
+		//Capitulo 02
+		
+		//Questao 01
+		String Questao_01 = "Hello Worlds Forever!";
+		System.out.println(Questao_01);
+		//Questão 02.
+		System.out.println("Hello");
+		System.out.println("World!");
+		//Questão 03.
+		System.out.println("Hello\nWorld!");
+	}
+	}
